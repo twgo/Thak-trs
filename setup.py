2c738a564b922b369980e8f3c5250826d6a3e6f4
fafoy <ihcaoe@gmail.com>
2018-07-13 15:46:49 +0800

    加setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+
+setup(
+    name='thaktrs',
+    packages=find_packages('trs'),
+    version='0.1.0',
+    author_email='ihcaoe@gmail.com',
+    url='https://xn--v0qr21b.xn--kpry57d/',
+    keywords=[
+        '自然語言', '語料庫',
+         'Natural Language', 'Corpus',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: Linguistic',
+    ],
+    install_requires=[
+    ],
+)
